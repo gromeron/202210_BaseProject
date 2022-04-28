069400190d2cc98ce72a081c428c42188b99cf8e
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-04-28 16:48:40 -0500

    class update

---
+++ src/app/modules/coffee/coffee.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoffeeRoutingModule } from './coffee-routing.module';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
+
+
+@NgModule({
+  declarations: [
+    CoffeeListComponent
+  ],
+  imports: [
+    CommonModule,
+    CoffeeRoutingModule
+  ]
+})
+export class CoffeeModule { }
