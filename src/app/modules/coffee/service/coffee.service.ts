eddd6c50b1bbd7fe076181796fba2fc3971c679c
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-04-28 17:24:18 -0500

    service update

--- src/app/modules/coffee/service/coffee.service.ts
+++ src/app/modules/coffee/service/coffee.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Coffee } from '../model/coffee';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,9 @@ export class CoffeeService {
 
   private readonly apiUrl: string = 'https://gist.githubusercontent.com/josejbocanegra/e9d24db370ce95b75555f7d1f8691805/raw/8a26ac2bca4183dc88545e14c45851d698911358/202212_MISW4104_Grupo3.json'
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getCoffees(): Observable<Coffee[]> {
+    return this._http.get<Coffee>(this.apiUrl);
+  }
 }
